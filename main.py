038325de82585885b16a303e374a97d86fb4ed69
syafiq <iqbal.syafiq.rozaan@gmail.com>
2025-03-07 22:49:30 +0700

    Refactor coffee addition process to use session state and improve form handling

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import streamlit as st
 from libs import Coffee, Producer
 
+if "taste" not in st.session_state:
+    st.session_state.taste = ""
+
 if "coffees" not in st.session_state:
     st.session_state.coffees = []
+
+def clear_form():
+    st.session_state.taste = ""
+    st.session_state.price = 0
+    st.session_state.producer_name = ""
+    st.session_state.producer_location = ""
+
+def submit():
+    producer = Producer(producer_name, producer_location)
+    coffee = Coffee(st.session_state.taste, price)
+    coffee.set_producer(producer)
+
+    st.session_state.coffees.append(coffee)
+    clear_form()
 
 st.title("Coffee Shop")
 
@@ -11,25 +28,11 @@ page = st.sidebar.selectbox("Choose a page", ["Add Coffee", "View Coffee"])
 
 if page == "Add Coffee":
     st.write("Add Coffee")
-    taste = st.text_input("Taste")
-    price = st.number_input("Price")
-    producer_name = st.text_input("Producer Name")
-    producer_location = st.text_input("Producer Location")
-    btn_add = st.button("Add Coffee")
-
-    if btn_add:
-        producer = Producer(producer_name, producer_location)
-        coffee = Coffee(taste, price)
-        coffee.set_producer(producer)
-        st.session_state.coffees.append(coffee)
-        
-        # Clear form inputs
-        st.session_state["taste"] = ""
-        st.session_state["price"] = 0.0
-        st.session_state["producer_name"] = ""
-        st.session_state["producer_location"] = ""
-        
-        st.rerun()
+    taste = st.text_input("Taste", key="taste")
+    price = st.number_input("Price", key="price")
+    producer_name = st.text_input("Producer Name", key="producer_name")
+    producer_location = st.text_input("Producer Location", key="producer_location")
+    btn_add = st.button("Add Coffee", on_click=submit)
 
 elif page == "View Coffee":
     if len(st.session_state.coffees) == 0:
@@ -42,6 +45,6 @@ elif page == "View Coffee":
             st.write(coffee.check_sold_out())
             btn_sold_out = st.button("Sold Out", key=f"btn_sold_out_{index}")
 
-            if btn_sold_out:
+            if btn_sold_out and not coffee.sold_out:
                 coffee.set_sold_out()
                 st.rerun()
